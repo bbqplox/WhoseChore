941667cff0e0dd7b30a6c480542c0bf29435f08c
Helen Cheng <hcheng@ucsd.edu>
2016-05-14 15:15:38 -0700

    login/signup accessible from homepage by helen

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     Rails.application.config.assets.precompile += %w( welcome.scss )
+    Rails.application.config.assets.precompile += %w( users.scss )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
