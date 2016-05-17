7283b692c6795ad4eafe795525462797746ba8f8
Joseph Widjaja <jewidjaj@ucsd.edu>
2016-05-16 19:25:21 -0700

    plss no crash

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,16 +1,16 @@
 class MembershipsController < ApplicationController
 ​
   def revoke
-    @membership.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Membership was revoked.' }
-      format.json { head :no_content }
-    end
+    #@membership.destroy
+    #respond_to do |format|
+      #format.html { redirect_to groups_url, notice: 'Membership was revoked.' }
+      #format.json { head :no_content }
+    #end
   end
 ​
   private
     def group_params
       params.require(:membership).permit(:group, :user)
     end
-    
+
 end
