d2a33e4cbf31c254493471d246060ee50dd6ac48
Joseph Widjaja <jewidjaj@ucsd.edu>
2016-05-16 19:42:08 -0700

    mac??

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,16 +1,13 @@
 class MembershipsController < ApplicationController
-​
-  #def revoke
-    #@membership.destroy
-    #respond_to do |format|
-      #format.html { redirect_to groups_url, notice: 'Membership was revoked.' }
-      #format.json { head :no_content }
-    #end
-  #end
-​
-  private
-    def membership_params
-      params.require(:membership).permit(:group, :user)
-    end
-
+#defrevoke
+#@membership.destroy
+#respond_to do |format|
+#format.html{redirect_to groups_url,notice:'Membership was revoked.'}
+#format.json{head:no_content}
+#end
+#end​
+private
+def membership_params
+params.require(:membership).permit(:group,:user)
+end
 end
