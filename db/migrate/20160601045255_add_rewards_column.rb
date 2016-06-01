3e7c1af38b7b5e069671d786c25ce6d6fc58cf4f
yeorj <gmalfaro@ucsd.edu>
2016-06-01 15:32:55 -0700

    Added histor of rewards

--- db/migrate/20160601045255_add_rewards_column.rb
+++ db/migrate/20160601045255_add_rewards_column.rb
@@ -2,6 +2,6 @@ class AddRewardsColumn < ActiveRecord::Migration
   def change
     add_column :rewards, :user_id, :integer, :default => -1
     add_column :rewards, :group_id, :integer
-    add_column :rewards, :claimed_time, :date
+    add_column :rewards, :claimed_time, :date, :default => Date.today
   end
 end
