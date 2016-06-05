84fd88c48153e9c07656b68990d5b5f8879500f6
yeorj <gmalfaro@ucsd.edu>
2016-06-04 18:20:50 -0700

    Fixed model bug

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,7 +2,7 @@ class Chore < ActiveRecord::Base
   #belongs_to :user
   belongs_to :group
   has_many :chore_rotations
-  validates :Name, :Description, :Date, :score, :presence => true
+  validates :name, :description, :date, :score, :presence => true
 
   def self.remind(chore_id)
     # TODO: add mail reminder for user
