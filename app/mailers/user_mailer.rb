ab7c008bcc630a3e0159696fb624ecc71e142af7
Tommy Truong <tttruong701@gmail.com>
2016-06-05 01:29:41 -0700

    Tommy testing cronjobs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,30 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'whosechore.herokuapp.com'
+    @url = 'whosechore2.herokuapp.com'
     mail(to: @user.email, subject: 'Welcome to whosechore!')
-    end
+  end
 
   def group_invite_email(user, group_name)
     @user = user
     @group_name = group_name
-    @url = 'whosechore.herokuapp.com'
+    @url = 'whosechore2.herokuapp.com'
     mail(to: @user.email, subject: 'Whosechore Group Invite')
-    end
+  end
+
+=begin
+  def test_email()
+    @url = 'whosechore.herokuapp.com'
+    mail(to: 'ttthao@ucsd.edu', subject: 'Welcome to whosechore!')
+  end
+=end
+
+  def chore_reminder_email(user, group, chore)
+    @user = user
+    @group = group
+    @chore = chore
+    @url = 'whosechore2.herokuapp.com'
+    mail(to: @user.email, subject: 'Whosechore Reminder!')
+  end
+
 end
--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,12 +3,20 @@ class Chore < ActiveRecord::Base
   belongs_to :group
   has_many :chore_rotations
 
-  def self.remind(chore_id)
-    # TODO: add mail reminder for user
+  def self.remind()
+    @incomplete_chores = incomplete()
+    for incomplete_chore in @incomplete_chores.each
+      if Date.today == Date.parse(incomplete_chore.date) - 1
+        @user = User.find(incomplete_chore.user_id)
+        @group = Group.find(incomplete_chore.group_id)
+        @chore = incomplete_chore
+        UserMailer.chore_reminder_email(@user, @group, @chore).deliver_now
+      end
+    end
   end
 
   def self.incomplete()
-    where(["completed = false"])
+    where(["complete = false"])
   end
 
   def self.destroy_all_completed(user_id)
